cb4cfa002817861677d558e16ba35452c0018116
Raiyan Jamil <73040210+Raiyanx@users.noreply.github.com>
2022-10-12 06:21:58 -0400

    minor changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 const User = require("./models/user")
 const dotenv = require("dotenv")
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 dotenv.config();
 
 app.set("view engine", "ejs")
